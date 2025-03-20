6576ea93280b48a3874d32a8ea76b846c1fde429
sandy-sp <sandeep.paidipati@gmail.com>
2025-03-20 13:48:57 -0400

    refactor(ui): delete reduntant ui files

--- src/gittxt-ui/frontend/src/components/ToastProvider.tsx
+++
@@ -1,7 +0,0 @@
-// src/components/ToastProvider.tsx
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-export default function ToastProvider() {
-  return <ToastContainer position="top-right" autoClose={3000} />;
-}
