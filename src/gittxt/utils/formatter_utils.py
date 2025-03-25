1fcab73a8507997cbfcaaf85822207ee0528d5d8
sandy-sp <sandeep.paidipati@gmail.com>
2025-03-25 08:55:07 -0400

    refactor(utils): simplify formatter to basic alphabetical file sorting

--- src/gittxt/utils/formatter_utils.py
+++ src/gittxt/utils/formatter_utils.py
@@ -1,16 +1,9 @@
+from pathlib import Path
 from gittxt.utils.filetype_utils import classify_simple
 
 def sort_textual_files(files):
-    priority = ["readme", "license", ".gitignore", "config", "docs", "code", "data"]
-    def file_priority(file):
-        fname = file.name.lower()
-        if fname.startswith("readme"):
-            return 0
-        if fname in {"license", "notice"}:
-            return 1
-        if fname in {".gitignore", ".dockerignore", ".gitattributes"}:
-            return 2
-        ext_priority = {"configs": 3, "docs": 4, "code": 5, "data": 6}
-        _, subcat = classify_simple(file)
-        return ext_priority.get(subcat, 7)
-    return sorted(files, key=file_priority)
+    """
+    Simple alphabetical sort for textual files.
+    You can enhance later with custom sort by size, name, etc.
+    """
+    return sorted(files, key=lambda f: f.name.lower())
