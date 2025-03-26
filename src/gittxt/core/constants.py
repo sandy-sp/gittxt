9571fe3f5b7ab2445db78d6efa9e6cb70809b3bd
sandy-sp <sandeep.paidipati@gmail.com>
2025-03-26 19:18:05 -0400

    refactor(core): unify folder paths and default exclude directories in constants.py

--- src/gittxt/core/constants.py
+++ src/gittxt/core/constants.py
@@ -1,8 +1,17 @@
-
 TEXT_DIR = "text"
 JSON_DIR = "json"
 MD_DIR = "md"
 ZIP_DIR = "zips"
 TEMP_DIR = "temp"
 
+# Potentially store default excludes in a single place, if you don’t want them in config.json
+DEFAULT_EXCLUDE_DIRS = [
+    ".git", 
+    "__pycache__", 
+    "node_modules", 
+    ".vscode", 
+    ".pytest_cache"
+]
+
+# If you want a quick reference to all subdirectories that might be cleaned:
 OUTPUT_SUBDIRS = [TEXT_DIR, JSON_DIR, MD_DIR, ZIP_DIR, TEMP_DIR]
