d7d6235c9dd5c61eb496777aac39bc548e62d229
sandy-sp <sandeep.paidipati@gmail.com>
2025-03-27 04:19:49 -0400

    chore(constants): define EXCLUDED_DIRS_DEFAULT for scanner

--- src/gittxt/core/constants.py
+++ src/gittxt/core/constants.py
@@ -5,17 +5,12 @@ ZIP_DIR = "zips"
 TEMP_DIR = "temp"
 
 # Potentially store default excludes in a single place, if you don’t want them in config.json
-DEFAULT_EXCLUDE_DIRS = [
-    ".git", 
-    "__pycache__", 
-    "node_modules", 
-    ".vscode", 
-    ".pytest_cache"
+EXCLUDED_DIRS_DEFAULT = [
+    ".git", "__pycache__", "node_modules", ".venv", "env", ".mypy_cache", ".pytest_cache", ".vscode", ".idea"
 ]
 
 # If you want a quick reference to all subdirectories that might be cleaned:
 OUTPUT_SUBDIRS = [TEXT_DIR, JSON_DIR, MD_DIR, ZIP_DIR, TEMP_DIR]
-
 
 # Updated config keys to textual_exts / non_textual_exts 
 DEFAULT_FILETYPE_CONFIG = {
