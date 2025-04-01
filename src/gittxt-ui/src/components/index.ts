6861a8e0c3055f124fe73011aeb806e3dd3502f5
sandy-sp <sandeep.paidipati@gmail.com>
2025-04-01 16:59:25 -0400

    chore(ui): export all UI components from a central index.ts file

---
+++ src/gittxt-ui/src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as ScanForm } from './ScanForm';
+export { default as SummaryPanel } from './SummaryPanel';
+export { default as TreeView } from './TreeView';
+export { default as FileTypeTable } from './FileTypeTable';
+export { default as DownloadPanel } from './DownloadPanel';
