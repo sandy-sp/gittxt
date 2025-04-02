5222fd5fe495e7c9a5de72fa51d080a8099c4de0
sandy-sp <sandeep.paidipati@gmail.com>
2025-04-02 09:14:58 -0400

    feat(FileTreeExplorer): replace ReactTooltip with Tooltip component for improved tooltip handling

--- src/gittxt-ui/src/components/FileTreeExplorer.tsx
+++ src/gittxt-ui/src/components/FileTreeExplorer.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Folder, FolderOpen, FileText } from 'lucide-react';
-import ReactTooltip from 'react-tooltip';
+import { Tooltip } from 'react-tooltip';
 import { getTooltipContent } from '../utils/tooltipHelpers';
 
 function TreeNode({
@@ -119,7 +119,7 @@ export default function FileTreeExplorer({
         data-tooltip-content={getTooltipContent(file, manifest)}>
         {file}
       </span>
-      <ReactTooltip place="right" type="dark" effect="solid" />
+      <Tooltip id="tooltip-id" place="right" type="dark" effect="solid" />
     </div>
   );
 }
