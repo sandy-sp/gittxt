115559af18dfe4afb1cbb9462db75318f137c665
sandy-sp <sandeep.paidipati@gmail.com>
2025-04-02 08:15:31 -0400

    feat(FileManifest): add interface for file manifest entries

---
+++ src/gittxt-ui/src/types/fileManifest.ts
@@ -0,0 +1,10 @@
+export interface FileManifestEntry {
+    path: string;
+    file_type: string;
+    language: string;
+    size_bytes: number;
+    size_readable: string;
+    token_count: number;
+    tokens_readable: string;
+  }
+  
