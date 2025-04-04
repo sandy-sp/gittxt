ba2b6a42a746e66a459e69be69e562cf497426f5
sandy-sp <sandeep.paidipati@gmail.com>
2025-04-04 03:53:25 -0400

    feat(gittxt_api): add download route to FastAPI application

--- src/gittxt_api/app/main.py
+++ src/gittxt_api/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from mangum import Mangum
 from gittxt_api.app.routes import scan
+from gittxt_api.app.routes import scan, download
 
 app = FastAPI(
     title="Gittxt API",
@@ -9,7 +10,8 @@ app = FastAPI(
 )
 
 # Include all route modules
-app.include_router(scan.router, prefix="")
+app.include_router(scan.router)
+app.include_router(download.router)
 
 # Entry point for AWS Lambda via API Gateway
 handler = Mangum(app)
