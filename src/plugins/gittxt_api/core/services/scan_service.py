6f3243cc4ab1c26c9137208cd2027c9d37ddb582
sandy-sp <sandeep.paidipati@gmail.com>
2025-04-17 14:38:12 -0400

    fix: update import paths in scan service

--- src/plugins/gittxt_api/core/services/scan_service.py
+++ src/plugins/gittxt_api/core/services/scan_service.py
@@ -7,8 +7,8 @@ from gittxt.utils.file_utils import load_gittxtignore
 from gittxt.utils.cleanup_utils import cleanup_temp_folder
 from gittxt.core.constants import EXCLUDED_DIRS_DEFAULT
 from gittxt.utils.summary_utils import generate_summary
-from gittxt_api.api.v1.models.scan_models import ScanRequest, ScanResponse
-from gittxt_api.api.v1.deps import get_output_dir
+from plugins.gittxt_api.api.v1.models.scan_models import ScanRequest, ScanResponse
+from plugins.gittxt_api.api.v1.deps import get_output_dir
 
 
 async def perform_scan(request: ScanRequest) -> ScanResponse:
