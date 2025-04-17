38b0cbfe0dccb3666f43a8275412b5275c6c4b4c
sandy-sp <sandeep.paidipati@gmail.com>
2025-04-17 16:08:50 -0400

    fix: await scan_id in test_download function

--- tests/api/test_download.py
+++ tests/api/test_download.py
@@ -3,6 +3,7 @@ import httpx
 
 @pytest.mark.asyncio
 async def test_download(scan_id):
+    scan_id = await scan_id 
     async with httpx.AsyncClient() as client:
         r = await client.get(f"http://127.0.0.1:8000/v1/download/{scan_id}?format=txt")
         assert r.status_code == 200
