38f415d1e5369d906029f503df89b8248f74a9bb
sandy-sp <sandeep.paidipati@gmail.com>
2025-04-30 22:08:08 -0400

    refactor: remove unused deps.py file from gittxt_web plugin

--- src/plugins/gittxt_web/api/v1/deps.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-from gittxt.core.config import ConfigManager
-
-def get_output_dir() -> Path:
-    config = ConfigManager.load_config()
-    return Path(config.get("output_dir", "./gittxt_output")).resolve()
