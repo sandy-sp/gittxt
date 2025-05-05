b182d6c0b986c4ad126e587b0936c8525cfac688
sandy-sp <sandeep.paidipati@gmail.com>
2025-05-05 18:39:22 -0400

    feat: implement CLI entry point to run the API using uvicorn

---
+++ gittxt_web/backend/__main__.py
@@ -0,0 +1,17 @@
+import uvicorn
+import click
+from gittxt_web.main import app
+
+@click.group()
+def cli(): ...
+
+@cli.command("run")
+@click.option("--host", default="0.0.0.0")
+@click.option("--port", default=8000, type=int)
+@click.option("--reload/--no-reload", default=True)
+def run(host, port, reload):
+    """Start the API."""
+    uvicorn.run(app, host=host, port=port, reload=reload)
+
+if __name__ == "__main__":
+    cli()
